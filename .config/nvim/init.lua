6b57a8dfd4fd0f3d7b5452b1d790c379c9421824
yenzzo <yenzzo.bucked251@passmail.net>
2026-01-16 19:03:36 -0300

    too lazy to explain

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,3 +4,7 @@ require("config.lazy")
 vim.opt.clipboard = "unnamedplus"
 
 vim.keymap.set({ "v" }, "<C-c>", "y")
+
+-- change '<<' and '>>' indentation number of spaces
+local o = vim.o
+o.shiftwidth = 3
