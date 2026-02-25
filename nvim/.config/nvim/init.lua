8a619ff7d281e148fc84f68332dbc5b6a21b830f
yenzzo <yenzzo.bucked251@passmail.net>
2026-02-25 15:42:15 -0300

    add some plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,19 @@
+-- set leader key to space
+vim.g.mapleader = ""
+vim.g.maplocalleader = ""
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop")
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.opt.clipboard = "unnamedplus"
+-- install mason
+require("mason").setup()
 
+-- install mason-lspconfig
+require("mason-lspconfig").setup()
+
+-- clipboard options
+vim.opt.clipboard = "unnamedplus"
 vim.keymap.set({ "v" }, "<C-c>", "y")
 
 -- change '<<' and '>>' indentation number of spaces
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,6 @@
 return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
-  { "rose-pine/neovim", name = "rose-pine" },
 
   -- configure LazyVim to load gruvbox
   {
